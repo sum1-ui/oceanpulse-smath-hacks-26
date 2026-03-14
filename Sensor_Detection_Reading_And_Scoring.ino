85eec6e4d14d7d513d0783455070274f3a5f0086
Kabir Kaul <kaulkabir@gmail.com>
2026-03-14 16:26:01 -0400

    Initial Commit and start on TDS sensor

---
+++ Sensor_Detection_Reading_And_Scoring.ino
@@ -0,0 +1,23 @@
+#include <EEPROM.h>
+#include "GravityTDS.h"
+
+
+//pin layout
+#define TdsSensorPin A1
+GravityTDS gravityTds;
+
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void calculateTotalDissolvedSolids() {
+
+}
